51fd015dd0d947fe0c0c61fa8ce0f351f44d5547
FantomFrenzy <ronieantonioarts@gmail.com>
2026-06-01 17:00:14 -0700

    one line testing

--- js/site.js
+++ js/site.js
@@ -3,17 +3,7 @@
 // Date:
 
 // Constants
-const textToType = `Throughout my years, I have shaped my work through the 
-growing tensions between realism and distortion, attempting to capture 
-both the harshness and the fragility of hope within the world among us. 
-
-Across various mediums, I find myself returning to themes of realism 
-through a darker lens, where monochromatic-esque palettes (black, white 
-and several muted tones) help strip scenes down to their emotional cores. 
-
-This reduction in color allows for mood, contrast, and form to carry 
-more weight in its meaning, often pushing ordinary subject matters into 
-something more unsettling, something that is eerily uncanny to the human eye.`;
+const textToType = `Throughout my years, I have shaped my work through the growing tensions between realism and distortion, attempting to capture both the harshness and the fragility of hope within the world among us. Across various mediums, I find myself returning to themes of realism through a darker lens, where monochromatic-esque palettes (black, white and several muted tones) help strip scenes down to their emotional cores. This reduction in color allows for mood, contrast, and form to carry more weight in its meaning, often pushing ordinary subject matters into something more unsettling, something that is eerily uncanny to the human eye.`;
 
 let index = 0;
 const speed = 30; 
